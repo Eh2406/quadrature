bca463a09af75f22b5d62e5b9912695937c38a49
Eh2406 <YeomanYaacov@gmail.com>
2016-12-11 21:24:02 -0500

    derect port

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a port of the [Fast Numerical Integration](https://www.codeproject.com/kb/recipes/fastnumericalintegration.aspx) from c++ to rust. The original code is by John D. Cook, and is licensed under the [BSD](https://opensource.org/licenses/bsd-license.php) Other quadrature techniques may be added in the future.
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+mod double_exponential_integration_constants;
+use double_exponential_integration_constants::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Output {
+    pub num_function_evaluations: u32,
+    pub error_estimate: f64,
+    pub integral: f64,
+}
+
+/// Integrate an analytic function over a finite interval.
+/// f is the function to be integrated.
+/// a is left limit of integration.
+/// b is right limit of integration
+/// target_absolute_error is the desired bound on error
+pub fn integrate<F>(f: F, a: f64, b: f64, target_absolute_error: f64) -> Output
+    where F: Fn(f64) -> f64
+{
+    // Apply the linear change of variables x = ct + d
+    // $$\int_a^b f(x) dx = c \int_{-1}^1 f( ct + d ) dt$$
+    // c = (b-a)/2, d = (a+b)/2
+    integrate_core(f, 0.5 * (b - a), 0.5 * (a + b), target_absolute_error)
+}
+
+/// Integrate f(cx + d) with the given integration constants
+fn integrate_core<F>(f: F, c: f64, d: f64, target_absolute_error: f64) -> Output
+    where F: Fn(f64) -> f64
+{
+    let mut error_estimate = ::std::f64::MAX;
+    let mut h = 1.0;
+    let mut num_function_evaluations = 0;
+    let mut previous_delta;
+    let mut current_delta = ::std::f64::MAX;
+
+    let mut integral = f(c * DOUBLE_EXPONENTIAL_ABCISSAS[0] + d) * DOUBLE_EXPONENTIAL_WEIGHTS[0];
+    num_function_evaluations += 1;
+
+    for i in DOUBLE_EXPONENTIAL_OFFSETS[0]..DOUBLE_EXPONENTIAL_OFFSETS[1] {
+        integral += DOUBLE_EXPONENTIAL_WEIGHTS[i] *
+                    (f(c * DOUBLE_EXPONENTIAL_ABCISSAS[i] + d) +
+                     f(-c * DOUBLE_EXPONENTIAL_ABCISSAS[i] + d));
+    }
+    num_function_evaluations += DOUBLE_EXPONENTIAL_OFFSETS[1] - DOUBLE_EXPONENTIAL_OFFSETS[0];
+
+    for level in 1..(DOUBLE_EXPONENTIAL_OFFSETS.len() - 1) {
+        h *= 0.5;
+        let mut new_contribution = 0.0;
+        for i in DOUBLE_EXPONENTIAL_OFFSETS[level]..DOUBLE_EXPONENTIAL_OFFSETS[level + 1] {
+            new_contribution += DOUBLE_EXPONENTIAL_WEIGHTS[i] *
+                                (f(c * DOUBLE_EXPONENTIAL_ABCISSAS[i] + d) +
+                                 f(-c * DOUBLE_EXPONENTIAL_ABCISSAS[i] + d));
+        }
+        num_function_evaluations += DOUBLE_EXPONENTIAL_OFFSETS[level] -
+                                    DOUBLE_EXPONENTIAL_OFFSETS[level];
+        new_contribution *= h;
+
+        // difference in consecutive integral estimates
+        previous_delta = current_delta;
+        current_delta = (0.5 * integral - new_contribution).abs();
+        integral = 0.5 * integral + new_contribution;
+
+        // Once convergence kicks in, error is approximately squared at each step.
+        // Determine whether we're in the convergent region by looking at the trend in the error.
+        if level == 1 {
+            continue; // previousDelta meaningless, so cannot check convergence.
+        }
+
+        // Exact comparison with zero is harmless here.  Could possibly be replaced with
+        // a small positive upper limit on the size of currentDelta, but determining
+        // that upper limit would be difficult.  At worse, the loop is executed more
+        // times than necessary.  But no infinite loop can result since there is
+        // an upper bound on the loop variable.
+        if current_delta == 0.0 {
+            break;
+        }
+        // previousDelta != 0 or would have been kicked out previously
+        let r = current_delta.ln() / previous_delta.ln();
+
+        if r > 1.9 && r < 2.1 {
+            // If convergence theory applied perfectly, r would be 2 in the convergence region.
+            // r close to 2 is good enough. We expect the difference between this integral estimate
+            // and the next one to be roughly delta^2.
+            error_estimate = previous_delta * previous_delta;
+        } else {
+            // Not in the convergence region.  Assume only that error is decreasing.
+            error_estimate = current_delta;
+        }
+
+        if error_estimate < 0.1 * target_absolute_error {
+            break;
+        }
+
+    }
+
+    Output {
+        num_function_evaluations: num_function_evaluations as u32,
+        error_estimate: c * error_estimate,
+        integral: c * integral,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn demo_function1(x: f64) -> f64 {
+        (-x / 5.0).exp() * x.powf(-1.0 / 3.0)
+    }
+    #[test]
+    fn demo_function1_works() {
+        let o = integrate(demo_function1, 0.0, 10.0, 1e-6);
+        assert!(o.error_estimate <= 1e-6, "error_estimate larger then asked");
+        assert!((o.integral - 3.6798142583691758).abs() <= o.error_estimate,
+                "error larger error_estimate");
+    }
+
+    fn demo_function2(x: f64) -> f64 {
+        (1.0 - x).powf(5.0) * x.powf(-1.0 / 3.0)
+    }
+    #[test]
+    fn demo_function2_works() {
+        let o = integrate(demo_function2, 0.0, 1.0, 1e-6);
+        assert!(o.error_estimate <= 1e-6, "error_estimate larger then asked");
+        assert!((o.integral - 0.41768525592055004).abs() <= o.error_estimate,
+                "error larger error_estimate");
+    }
+
+    fn demo_bad_function1(x: f64) -> f64 {
+        (1.0 - x).powf(0.99)
+    }
+    #[test]
+    fn demo_bad_function1_works() {
+        let o = integrate(demo_bad_function1, 0.0, 1.0, 1e-6);
+        assert!(o.error_estimate <= 1e-6, "error_estimate larger then asked");
+        assert!((o.integral - 0.50251256281407035).abs() <= o.error_estimate,
+                "error larger error_estimate");
+    }
+
+    fn demo_bad_function2(x: f64) -> f64 {
+        x.abs()
+    }
+    #[test]
+    fn demo_bad_function2_works() {
+        let o = integrate(demo_bad_function2, -1.0, 1.0, 1e-6);
+        assert!((o.integral - 1.0).abs() <= o.error_estimate,
+                "error larger error_estimate");
+    }
+
+    fn demo_bad_function3(x: f64) -> f64 {
+        (0.5 - x.abs()).abs()
+    }
+    #[test]
+    fn demo_bad_function3_works() {
+        let o = integrate(demo_bad_function3, -1.0, 1.0, 1e-6);
+        assert!((o.integral - 0.5).abs() <= o.error_estimate,
+                "error larger error_estimate");
+    }
+}
