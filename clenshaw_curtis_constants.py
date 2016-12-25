73d460b6809e6e5d75fdf42340e4df7ffc629597
Eh2406 <YeomanYaacov@gmail.com>
2016-12-25 12:51:28 -0500

    Clenshaw-Curtis cheked

--- clenshaw_curtis_constants.py
+++ clenshaw_curtis_constants.py
@@ -28,11 +28,43 @@ def test_with_sympy(n):
     print sum([w*f.subs(x, xi) for xi, w in zip(*weights(n))])
     print sympy.integrate(f, (x, -1, 1))
 
+def clencurt(n1):
+    """ Computes the Clenshaw Curtis nodes and weights """
+    # http://www.scientificpython.net/pyblog/clenshaw-curtis-quadrature
+    if n1 == 1:
+        x = 0.
+        w = 2.
+    if n1 == 2:
+        x = [-1., 1.]
+        w = [1., 1.]
+    else:
+        n = n1 - 1
+        C = np.zeros((n1,2))
+        k = 2*(1+np.arange(np.floor(n/2)))
+        C[::2,0] = 2/np.hstack((1, 1-k*k))
+        C[1,1] = -n
+        V = np.vstack((C,np.flipud(C[1:n,:])))
+        F = np.real(np.fft.ifft(V, n=None, axis=0))
+        x = F[0:n1,1]
+        w = np.hstack((F[0,0],2*F[1:n,0],F[n,0]))
+    return x, w
 
 for i in range(1, 5):
     n = 2**i+ 1
-    print n, weights(n)[0][n /2 :]
-    print n, weights(n)[1][n /2 :]
+    o_x = weights(n)[0][n /2 :]
+    o_w = weights(n)[1][n /2 :]
+    print "o_x", n, o_x
+    print "o_w", n, o_w
+    n = 2**(i+1)+ 1
+    n_x = weights(n)[0][n /2 :]
+    n_w = weights(n)[1][n /2 :]
+    print "n_x", n, n_x
+    print "n_w", n, n_w
+    on_x = n_x[::2]
+    on_w = n_w[::2]
+    print "on_x", n, on_x
+    print "on_w", n, on_w
+    print "on_w/o_w", n, on_w/o_w
+    # on_w/o_w is not a constant so we do need to hold the old f calls
+    # looks like we can reuse x's but not w's
     print
-
-# looks like we can reuse x's but not w
--- double_exponential_constants.py
+++ double_exponential_constants.py
@@ -2,38 +2,62 @@
 
 import numpy as np
 
-# Double Exponential Formulas for Numerical Integration By Hidetosi TAKAHASI* and Masatake MORI (3-3)
-# Integration(f(x), -1, 1) ~=
-# pi/2 * h * sum(f(tanh(pi/2 * sinh(n*h)))*(cosh(n*h)/ (cosh(pi/2 * sinh(n*h)))^2 ), n, -oo, oo)
+def weights(n):
+    # Double Exponential Formulas for Numerical Integration By Hidetosi TAKAHASI* and Masatake MORI (3-3)
+    # Integration(f(x), -1, 1) ~=
+    # pi/2 * h * sum(f(tanh(pi/2 * sinh(n*h)))*(cosh(n*h)/ (cosh(pi/2 * sinh(n*h)))^2 ), n, -oo, oo)
 
-# so the ABCISSAS are:
-# tanh(pi/2 * sinh(n*h)
+    # so the ABCISSAS are:
+    # tanh(pi/2 * sinh(n*h)
 
-# and the WEIGHTS are:
-# pi/2 * h *(cosh(n*h)/ (cosh(pi/2 * sinh(n*h)))^2
+    # and the WEIGHTS are:
+    # pi/2 * h *(cosh(n*h)/ (cosh(pi/2 * sinh(n*h)))^2
 
-# John D. Cook uses -oo = -3 and oo = 3
+    # John D. Cook uses -oo = -3 and oo = 3
+
+    u = np.linspace(-3, 3, n)
+    h = u[1] - u[0]
+    x = np.tanh(np.pi/2 * np.sinh(u))
+    w = np.pi/2.0 * h *(np.cosh(u)/ ((np.cosh(np.pi/2 * np.sinh(u)))**2))
+    return x, w
 
 # and 1st layer has 7 fn cals so
-u1 = np.linspace(-3, 3, 7)
-h1 = u1[1] - u1[0]
-ABCISSAS1 = np.tanh(np.pi/2 * np.sinh(u1))
-print ABCISSAS1[int(len(ABCISSAS1)/2):]
-WEIGHTS1 = np.pi/2.0 * h1 *(np.cosh(u1)/ ((np.cosh(np.pi/2 * np.sinh(u1)))**2))
-print WEIGHTS1[int(len(WEIGHTS1)/2):]
+n = 7
+print weights(n)[0][n/2:]
+print weights(n)[1][n/2:]
 
 # and 2st layer has 6 fn cals more
-u2 = np.linspace(-3, 3, len(u1) + 6)
-h2 = u2[1] - u2[0]
-ABCISSAS2 = np.tanh(np.pi/2 * np.sinh(u2))
-print ABCISSAS2[int(len(ABCISSAS2)/2):]
-WEIGHTS2 = np.pi/2.0 * h2 *(np.cosh(u2)/ ((np.cosh(np.pi/2 * np.sinh(u2)))**2))
-print WEIGHTS2[int(len(WEIGHTS2)/2):]
+n += 6
+print weights(n)[0][n/2:]
+print weights(n)[1][n/2:]
 
 # and 3st layer has 12 fn cals more
-u3 = np.linspace(-3, 3, len(u2) + 12)
-h3 = u3[1] - u3[0]
-ABCISSAS3 = np.tanh(np.pi/2 * np.sinh(u3))
-print ABCISSAS3[int(len(ABCISSAS3)/2):]
-WEIGHTS3 = np.pi/2.0 * h3 *(np.cosh(u3)/ ((np.cosh(np.pi/2 * np.sinh(u3)))**2))
-print WEIGHTS3[int(len(WEIGHTS3)/2):]
+n += 12
+print weights(n)[0][n/2:]
+print weights(n)[1][n/2:]
+
+def test_with_sympy(n):
+    import sympy;
+    a, b, c, x = sympy.symbols("a, b, c, x")
+    f = a* x **2 + b * x + c
+    print sum([w*f.subs(x, xi) for xi, w in zip(*weights(n))])
+    print sympy.integrate(f, (x, -1, 1))
+
+for i in range(1, 5):
+    n = 2**i+ 1
+    o_x = weights(n)[0][n /2 :]
+    o_w = weights(n)[1][n /2 :]
+    print "o_x", n, o_x
+    print "o_w", n, o_w
+    n = 2**(i+1)+ 1
+    n_x = weights(n)[0][n /2 :]
+    n_w = weights(n)[1][n /2 :]
+    print "n_x", n, n_x
+    print "n_w", n, n_w
+    on_x = n_x[::2]
+    on_w = n_w[::2]
+    print "on_x", n, on_x
+    print "on_w", n, on_w
+    print "on_w/o_w", n, on_w/o_w
+    # on_w/o_w is a constant so we do not need to hold the old f calls
+    print
