54785b5943d924b2e3a7468cb4650ac1dab2a57b
Eh2406 <YeomanYaacov@gmail.com>
2017-01-03 15:57:21 -0500

    format python

--- clenshaw_curtis_constants.py
+++ clenshaw_curtis_constants.py
@@ -1,11 +1,12 @@
 # http://www.sam.math.ethz.ch/~waldvoge/Papers/fejer.pdf
 import numpy as np
 
+
 # 2.4
 def weights(n):
     n -= 1
     v = np.linspace(0, 1, n + 1) * np.pi
-    j = np.array(range(1, n / 2 + 1))[:,None]
+    j = np.array(range(1, n / 2 + 1))[:, None]
 
     b = 2. * np.ones(j.shape)
     b -= (j == n / 2.)
@@ -16,17 +17,19 @@ def weights(n):
 
     s = (b / (4. * j ** 2 - 1) * np.cos(2 * j * v)).sum(0)
     x = (np.cos(v) + 1.0) - 1.0
-    w = c  / n * (1. - s)
-    assert np.abs(w[0] - 1./(n ** 2. - 1. + (n % 2))) < 1e-12
-    assert np.abs(w[-1] - 1./(n ** 2. - 1. + (n % 2))) < 1e-12
+    w = c / n * (1. - s)
+    assert np.abs(w[0] - 1. / (n ** 2. - 1. + (n % 2))) < 1e-12
+    assert np.abs(w[-1] - 1. / (n ** 2. - 1. + (n % 2))) < 1e-12
     return x, w
 
+
 def test_with_sympy(n):
-    import sympy;
+    import sympy
     a, b, c, x = sympy.symbols("a, b, c, x")
-    f = a* x **2 + b * x + c
-    print sum([w*f.subs(x, xi) for xi, w in zip(*weights(n))])
+    f = a * x ** 2 + b * x + c
+    print sum([w * f.subs(x, xi) for xi, w in zip(*weights(n))])
     print sympy.integrate(f, (x, -1, 1))
+
 
 def clencurt(n1):
     """ Computes the Clenshaw Curtis nodes and weights """
@@ -39,32 +42,33 @@ def clencurt(n1):
         w = [1., 1.]
     else:
         n = n1 - 1
-        C = np.zeros((n1,2))
-        k = 2*(1+np.arange(np.floor(n/2)))
-        C[::2,0] = 2/np.hstack((1, 1-k*k))
-        C[1,1] = -n
-        V = np.vstack((C,np.flipud(C[1:n,:])))
+        C = np.zeros((n1, 2))
+        k = 2 * (1 + np.arange(np.floor(n / 2)))
+        C[::2, 0] = 2 / np.hstack((1, 1 - k * k))
+        C[1, 1] = -n
+        V = np.vstack((C, np.flipud(C[1:n, :])))
         F = np.real(np.fft.ifft(V, n=None, axis=0))
-        x = F[0:n1,1]
-        w = np.hstack((F[0,0],2*F[1:n,0],F[n,0]))
+        x = F[0:n1, 1]
+        w = np.hstack((F[0, 0], 2 * F[1:n, 0], F[n, 0]))
     return x, w
 
+
 for i in range(1, 5):
-    n = 2**i+ 1
-    o_x = weights(n)[0][n /2 :]
-    o_w = weights(n)[1][n /2 :]
+    n = 2 ** i + 1
+    o_x = weights(n)[0][n / 2:]
+    o_w = weights(n)[1][n / 2:]
     print "o_x", n, o_x
     print "o_w", n, o_w
-    n = 2**(i+1)+ 1
-    n_x = weights(n)[0][n /2 :]
-    n_w = weights(n)[1][n /2 :]
+    n = 2 ** (i + 1) + 1
+    n_x = weights(n)[0][n / 2:]
+    n_w = weights(n)[1][n / 2:]
     print "n_x", n, n_x
     print "n_w", n, n_w
     on_x = n_x[::2]
     on_w = n_w[::2]
     print "on_x", n, on_x
     print "on_w", n, on_w
-    print "on_w/o_w", n, on_w/o_w
+    print "on_w/o_w", n, on_w / o_w
     # on_w/o_w is not a constant so we do need to hold the old f calls
     # looks like we can reuse x's but not w's
     print
--- double_exponential_constants.py
+++ double_exponential_constants.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 def weights(n):
     # Double Exponential Formulas for Numerical Integration By Hidetosi TAKAHASI* and Masatake MORI (3-3)
     # Integration(f(x), -1, 1) ~=
@@ -17,47 +18,50 @@ def weights(n):
 
     u = np.linspace(-3, 3, n)
     h = u[1] - u[0]
-    x = np.tanh(np.pi/2 * np.sinh(u))
-    w = np.pi/2.0 * h *(np.cosh(u)/ ((np.cosh(np.pi/2 * np.sinh(u)))**2))
+    x = np.tanh(np.pi / 2 * np.sinh(u))
+    w = np.pi / 2.0 * h * (np.cosh(u) / ((np.cosh(np.pi / 2 * np.sinh(u))) ** 2))
     return x, w
+
 
 # and 1st layer has 7 fn cals so
 n = 7
-print weights(n)[0][n/2:]
-print weights(n)[1][n/2:]
+print weights(n)[0][n / 2:]
+print weights(n)[1][n / 2:]
 
 # and 2st layer has 6 fn cals more
 n += 6
-print weights(n)[0][n/2:]
-print weights(n)[1][n/2:]
+print weights(n)[0][n / 2:]
+print weights(n)[1][n / 2:]
 
 # and 3st layer has 12 fn cals more
 n += 12
-print weights(n)[0][n/2:]
-print weights(n)[1][n/2:]
+print weights(n)[0][n / 2:]
+print weights(n)[1][n / 2:]
+
 
 def test_with_sympy(n):
-    import sympy;
+    import sympy
     a, b, c, x = sympy.symbols("a, b, c, x")
-    f = a* x **2 + b * x + c
-    print sum([w*f.subs(x, xi) for xi, w in zip(*weights(n))])
+    f = a * x ** 2 + b * x + c
+    print sum([w * f.subs(x, xi) for xi, w in zip(*weights(n))])
     print sympy.integrate(f, (x, -1, 1))
 
+
 for i in range(1, 5):
-    n = 2**i+ 1
-    o_x = weights(n)[0][n /2 :]
-    o_w = weights(n)[1][n /2 :]
+    n = 2 ** i + 1
+    o_x = weights(n)[0][n / 2:]
+    o_w = weights(n)[1][n / 2:]
     print "o_x", n, o_x
     print "o_w", n, o_w
-    n = 2**(i+1)+ 1
-    n_x = weights(n)[0][n /2 :]
-    n_w = weights(n)[1][n /2 :]
+    n = 2 ** (i + 1) + 1
+    n_x = weights(n)[0][n / 2:]
+    n_w = weights(n)[1][n / 2:]
     print "n_x", n, n_x
     print "n_w", n, n_w
     on_x = n_x[::2]
     on_w = n_w[::2]
     print "on_x", n, on_x
     print "on_w", n, on_w
-    print "on_w/o_w", n, on_w/o_w
+    print "on_w/o_w", n, on_w / o_w
     # on_w/o_w is a constant so we do not need to hold the old f calls
     print
